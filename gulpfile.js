4a4f2e269689dc60fcd952fd52a13eb9d48e6697
desirelabs <francklebas@protonmail.ch>
2017-02-26 09:35:12 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var gulp = require('gulp');
 // Include plugins
 var plugins = require('gulp-load-plugins')();
 var livereload = require('gulp-livereload');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var babel = require('gulp-babel');
+var watch = require('gulp-watch')
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
@@ -38,3 +42,37 @@ gulp.task('watch', function () {
 
 // Tâche par défaut
 gulp.task('default', ['build']);
+
+
+/**
+ * JS
+ */
+var distDir = 'dist/'
+var srcDir = 'js/**/*.js'
+
+// Compresse les js
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('js/**/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+function runBabel() {
+    return gulp.src(srcDir)
+        .pipe(babel())
+        .pipe(gulp.dest(distDir))
+}
+
+gulp.task('babel', () => runBabel());
+
+gulp.task('babel-watch', ['babel'], cb => {
+    watch(srcDir, () =>
+        runBabel()
+            .pipe(watch(srcDir))
+            .on('end', cb)
+    )
+})
